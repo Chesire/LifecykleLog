ef2cce904b8a42b83fbcbd2f88c6ffce17198c49
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-22 21:58:24 +0000

    feat: add fragment to the sample application

--- lifecyklelog-sample/src/main/java/com/chesire/lifecyklelogsample/MainActivity.kt
+++ lifecyklelog-sample/src/main/java/com/chesire/lifecyklelogsample/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.activityMainContainer, MainFragment.newInstance())
+                .commit()
+        }
     }
 }
---
+++ lifecyklelog-sample/src/main/java/com/chesire/lifecyklelogsample/MainFragment.kt
@@ -0,0 +1,19 @@
+package com.chesire.lifecyklelogsample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_main, container, false)
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+}
