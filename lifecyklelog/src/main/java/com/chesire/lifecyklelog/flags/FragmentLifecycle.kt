63aebba9e6afa02df56af5307c934a19b05557ac
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-23 12:23:16 +0000

    feat: add enum class for fragment lifecycle methods

---
+++ lifecyklelog/src/main/java/com/chesire/lifecyklelog/flags/FragmentLifecycle.kt
@@ -0,0 +1,19 @@
+package com.chesire.lifecyklelog.flags
+
+enum class FragmentLifecycle(val lifecycleEvent: String) {
+    ON_ATTACH("onAttach"),
+    ON_CREATE("onCreate"),
+    ON_CREATE_VIEW("onCreateView"),
+    ON_ACTIVITY_CREATED("onActivityCreated"),
+    ON_START("onStart"),
+    ON_RESUME("onResume"),
+
+    ON_PAUSE("onPause"),
+    ON_STOP("onStop"),
+    ON_DESTROY_VIEW("onDestroyView"),
+    ON_DESTROY("onDestroy"),
+    ON_DETACH("onDetach"),
+
+    ON_PRE_CREATED("onPreCreated"),
+    ON_SAVE_INSTANCE_STATE("onSaveInstanceState")
+}
