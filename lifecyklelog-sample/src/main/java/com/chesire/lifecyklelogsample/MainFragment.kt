1479fbd98b07e9b181aed7a99b4dc96047393543
Chesire <Chesire@users.noreply.github.com>
2020-12-22 22:47:22 +0000

    chore: update sample code (#145)
    Update the sample code to be a little less verbose, and be more
    up to date. No functional changes, just some minor tweaks.

--- lifecyklelog-sample/src/main/java/com/chesire/lifecyklelogsample/MainFragment.kt
+++ lifecyklelog-sample/src/main/java/com/chesire/lifecyklelogsample/MainFragment.kt
@@ -1,9 +1,5 @@
 package com.chesire.lifecyklelogsample
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.chesire.lifecyklelog.LifecycleEvent
 import com.chesire.lifecyklelog.LogLifecykle
@@ -19,13 +15,7 @@ import com.chesire.lifecyklelog.LogLifecykle
         LifecycleEvent.ON_DESTROY
     ]
 )
-class MainFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_main, container, false)
-
+class MainFragment : Fragment(R.layout.fragment_main) {
     companion object {
         /**
          * Generate a new instance of [MainFragment].
